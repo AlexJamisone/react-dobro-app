8f214fd5200f575f354dc7a3d8f3f5370392627d
AlexJamisone <business.homeit@gmail.com>
2022-07-01 16:42:12 +0300

    finish responsive

--- src/components/Menu/MenuToggle/MenuToggle.jsx
+++ src/components/Menu/MenuToggle/MenuToggle.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { motion } from 'framer-motion';
 
+import './MenuToggle.scss'
+
 const Path = props => (
     <motion.path
       fill="transparent"
@@ -12,7 +14,7 @@ const Path = props => (
   );
   
   export const MenuToggle = ({ toggle }) => (
-    <button onClick={toggle}>
+    <button onClick={toggle} className='btn-burger'>
       <svg width="23" height="23" viewBox="0 0 23 23">
         <Path
           variants={{

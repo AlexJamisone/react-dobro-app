e10d6020f98769dee6d93e1102c5ef1de1a86cbf
AlexJamisone <business.homeit@gmail.com>
2022-07-16 14:47:34 +0300

    create link to app for delivery

--- src/components/LinkToApp/LinkToApp.jsx
+++ src/components/LinkToApp/LinkToApp.jsx
@@ -1,11 +1,31 @@
 import './LinkToApp.scss'
-import { Link } from 'react-router-dom'
+import PlayStore from '../../assets/googleplay.png'
+import AppStore from '../../assets/appstore.png'
+
 const LinkToApp = () => {
-  return (
-    <div className='link-container'>
-        Content
-    </div>
-  )
+	return (
+		<div className='link-container'>
+		<h1 className='link-text'> Привет, у нас появилось приложение с доставкой!</h1>
+			<a 
+				className='link-playStore'
+				href='https://play.google.com/store/apps/details?id=ru.quickresto.loyalty.dobrocoffeecr'
+				target='_blank'
+				rel='noreferrer'
+			>
+				<img src={PlayStore} alt="Play Market" />
+			</a>
+			<a 
+				className='link-appStore'
+				href='https://play.google.com/store/apps/details?id=ru.quickresto.loyalty.dobrocoffeecr'
+				target='_blank'
+				rel='noreferrer'
+				
+			>
+				<img src={AppStore} alt="Play Market" />
+			</a>
+		</div>
+
+	)
 }
 
 export default LinkToApp

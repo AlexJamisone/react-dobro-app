e41f4523de72eeea35de49aac7153051cc953ba3
AlexJamisone <business.homeit@gmail.com>
2022-07-18 22:02:54 +0300

    create router of Tips

---
+++ src/routes/Tips/Tips.jsx
@@ -0,0 +1,14 @@
+import './Tips.scss'
+import Background from '../../container/Background/Background'
+import { Navigation } from '../../components'
+
+const Tips = () => {
+  return (
+    <div>
+        <Background/>
+        <Navigation/>
+    </div>
+  )
+}
+
+export default Tips
